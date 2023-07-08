33e9b999f2be388581098f66ed29db13415e3201
itochs <halglobe0108@gmail.com>
2023-07-08 15:15:27 +0900

    union by size

--- union-find/src/main.rs
+++ union-find/src/main.rs
@@ -21,19 +21,17 @@ impl UnionFind {
     // uとvの結合
     // 値を変更するのでmutとしてselfを参照
     fn unite(&mut self, u: usize, v: usize) -> bool {
-        let ru = self.root(u);
-        let rv = self.root(v);
-        if ru == rv {
+        if self.root(u) == self.root(v) {
             return false;
         }
         // union by size
         // ノード数が大きい方に，ノード数が小さい方を結合する
-        if self.size[rv] < self.size[ru] {
-            self.parent[ru] = rv;
-            self.size[ru] += self.size[rv];
+        if self.size[u] < self.size[v] {
+            self.parent[u] = v;
+            self.size[v] += self.size[u];
         } else {
-            self.parent[rv] = ru;
-            self.size[rv] += self.size[ru];
+            self.parent[v] = u;
+            self.size[u] += self.size[v];
         }
 
         return true;
@@ -47,13 +45,14 @@ impl UnionFind {
 fn main() {
     println!("Hello, world!");
 
-    // {{0,1,2}, {3,4}, {5}, {6}}
+    // {{0,1,2,3}, {4,5}, {6}}
     let mut uf = UnionFind::new(7);
     uf.unite(0, 1);
     uf.unite(0, 2);
-    uf.unite(4, 3);
+    uf.unite(2, 3);
+    uf.unite(4, 5);
     for i in 0..7 {
-        println!("{} root: {}", i, uf.root(i));
+        println!("{} root: {}, parent: {}", i, uf.root(i), uf.parent[i]);
     }
     println!("1 0 is same?: {}", uf.same(1, 0));
     println!("5 6 is same?: {}", uf.same(5, 6));
